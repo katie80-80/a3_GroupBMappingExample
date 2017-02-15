fb7d5fb0171ec36af5108f4008206f854b2d5183
Kate Spence <katespence57@gmail.com>
2017-02-15 11:48:09 -0500

    initial files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+(function() {
+	"use strict";
+	console.log(" SEAF fired");
+	
+	//variables
+	var map, marker;
+	
+	//functions
+	function initMap(){
+		map = new google.maps.Map(document.querySelector('.map-wrapper'),
+			{
+				center: {lat:42.983233, lng: -81.250688},
+				zoom: 14
+			}
+		);
+
+		marker = new google.maps.Marker({
+			position : {lat:42.983233, lng: -81.250688},
+			map: map,
+			title: 'hello World!'
+		});
+	}
+	initMap();
+	//listeners
+	
+	})();
