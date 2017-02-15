6abad7b60201878e3eb75be73f205f69d6b4e56a
Kate Spence <katespence57@gmail.com>
2017-02-15 11:54:49 -0500

    made it work

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
 	function initMap(){
 		map = new google.maps.Map(document.querySelector('.map-wrapper'),
 			{
-				center: {lat:42.983233, lng: -81.250688},
+				center: { lat:42.983233, lng: -81.250688 },
 				zoom: 14
 			}
 		);
